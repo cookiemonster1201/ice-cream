d1a8f5c0f544dbdc57b06c365de44a5fd33f7dc1
Oleksandr Shapovaliuk <shapovaluak12052002@gmail.com>
2021-04-25 22:33:01 +0300

    bugfix/resolve conflict script

--- src/js/about-modal.js
+++ src/js/about-modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-about-modal-open]'),
+    closeModalBtn: document.querySelector('[data-about-modal-close]'),
+    modal: document.querySelector('[data-about-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
