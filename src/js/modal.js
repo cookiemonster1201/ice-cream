544cc7c3ef33af34ae57c9ae4a81a600ee4d3191
Oleksandr Shapovaliuk <shapovaluak12052002@gmail.com>
2021-04-26 16:11:31 +0300

    added anchors

--- src/js/modal.js
+++ src/js/modal.js
@@ -14,12 +14,11 @@
   }
 })();
 
-
 //СКРИПТ ДЛЯ МОДАЛЬНОГО ОКНА, ВЫЗЫВАЕМОГО ИЗ МОБИЛЬНОГО МЕНЮ
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-mob-open]'),
-    modal: document.querySelector('[data-modal-mob]'),
+    closeModalBtn: document.querySelector('[data-modal-mob]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
