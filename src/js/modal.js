2b7f210bd9182e47f670ce3d1e599ccab30cf166
Oleksandr Shapovaliuk <shapovaluak12052002@gmail.com>
2021-04-27 00:27:53 +0300

    resolve conflict

--- src/js/modal.js
+++ src/js/modal.js
@@ -19,11 +19,10 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-mob-open]'),
-    closeModalBtn: document.querySelector('[data-modal-mob]'),
+    modal: document.querySelector('[data-modal-mob]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
