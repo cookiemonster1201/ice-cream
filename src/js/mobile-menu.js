04338962cec384b38f227f7b96214f2dbcc5f24e
Vadym Polishchuk <polischukvadim@gmail.com>
2021-04-28 02:30:30 +0300

    Added script for close mob.menu after click on link

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -12,16 +12,18 @@
   });
 })();
 
-//СКРИПТ ДЛЯ СКРЫТИЯ БУРГЕР-МЕНЮ ПРИ ОТКРЫТИИ МОБ.МЕНЮ
+/*СКРИПТ ДЛЯ СКРЫТИЯ БУРГЕР-МЕНЮ ПРИ ОТКРЫТИИ МОБ.МЕНЮ И 
+ДЛЯ ЗАКРЫТИЯ МОБ. МЕНЮ ПОСЛЕ КЛИКА ПО ССЫЛКЕ */
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-btn]");
-  const mobileBtnClose = document.querySelector("[data-menu-close]");
+  const mobileMenuClose = document.querySelector("[data-menu-close]");
 
   menuBtnRef.addEventListener("click", () => {
     menuBtnRef.classList.toggle("is-active");
   });
 
-  mobileBtnClose.addEventListener("click", () => {
+  mobileMenuClose.addEventListener("click", () => {
     menuBtnRef.classList.toggle("is-active");
+    // mobileMenuClose.classList.toggle("is-close");
   });
 })();
