4fbb6f193bbdf51de9fd5981a765c5c960b50324
Oleksandr Shapovaliuk <shapovaluak12052002@gmail.com>
2021-04-28 09:57:49 +0300

    edit menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,29 +1,29 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
-  const mobileBtnClose = document.querySelector("[data-menu-close]");
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const mobileBtnClose = document.querySelector('[data-menu-close]');
 
-  menuBtnRef.addEventListener("click", () => {
-    mobileMenuRef.classList.toggle("is-open");
+  menuBtnRef.addEventListener('click', () => {
+    mobileMenuRef.classList.toggle('is-open');
   });
 
-  mobileBtnClose.addEventListener("click", () => {
-    mobileMenuRef.classList.toggle("is-open");
+  mobileBtnClose.addEventListener('click', () => {
+    mobileMenuRef.classList.toggle('is-open');
   });
 })();
 
 /*СКРИПТ ДЛЯ СКРЫТИЯ БУРГЕР-МЕНЮ ПРИ ОТКРЫТИИ МОБ.МЕНЮ И 
 ДЛЯ ЗАКРЫТИЯ МОБ. МЕНЮ ПОСЛЕ КЛИКА ПО ССЫЛКЕ */
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-btn]");
-  const mobileMenuClose = document.querySelector("[data-menu-close]");
+  const menuBtnRef = document.querySelector('[data-menu-btn]');
+  const mobileMenuClose = document.querySelector('[data-menu-close]');
 
-  menuBtnRef.addEventListener("click", () => {
-    menuBtnRef.classList.toggle("is-active");
+  menuBtnRef.addEventListener('click', () => {
+    menuBtnRef.classList.toggle('is-active');
   });
 
-  mobileMenuClose.addEventListener("click", () => {
-    menuBtnRef.classList.toggle("is-active");
-    // mobileMenuClose.classList.toggle("is-close");
+  mobileMenuClose.addEventListener('click', () => {
+    menuBtnRef.classList.toggle('is-active');
+    // mobileMenuClose.classList.toggle('is-close');
   });
 })();
