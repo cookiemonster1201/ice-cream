1e921e77c8085e5483fb8d0989ffd585113abb77
Oleksandr Shapovaliuk <shapovaluak12052002@gmail.com>
2021-04-28 20:45:11 +0300

    edit js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -5,6 +5,7 @@
 
   menuBtnRef.addEventListener('click', () => {
     mobileMenuRef.classList.toggle('is-open');
+    document.body.classList.toggle('modal-open');
   });
 
   mobileBtnClose.addEventListener('click', () => {
@@ -28,5 +29,6 @@
   mobileMenuClose.addEventListener('click', () => {
     menuBtnRef.classList.toggle('is-active');
     modalBtnRef.classList.toggle('is-visibility');
+    document.body.classList.toggle('modal-open');
   });
 })();
