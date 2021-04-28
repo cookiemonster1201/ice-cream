a076c0062acdc8d0885f3b73349cd0d420199607
Oleksandr Shapovaliuk <shapovaluak12052002@gmail.com>
2021-04-28 20:38:13 +0300

    edit script

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -15,17 +15,18 @@
 /*СКРИПТ ДЛЯ СКРЫТИЯ БУРГЕР-МЕНЮ И МОДАЛЬНОЙ КНОПКИ ПРИ ОТКРЫТИИ МОБ.МЕНЮ И 
 ДЛЯ ЗАКРЫТИЯ МОБ. МЕНЮ ПОСЛЕ КЛИКА ПО ССЫЛКЕ */
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-btn]");
-  const mobileMenuClose = document.querySelector("[data-menu-close]");
-  const modalBtnRef = document.querySelector("[data-modal-btn]");
+  const menuBtnRef = document.querySelector('[data-menu-btn]');
+  const mobileMenuClose = document.querySelector('[data-menu-close]');
+  const modalBtnRef = document.querySelector('[data-modal-btn]');
 
-  menuBtnRef.addEventListener("click", () => {
-    menuBtnRef.classList.toggle("is-active");
-    modalBtnRef.classList.toggle("is-visibility");
+  menuBtnRef.addEventListener('click', () => {
+    menuBtnRef.classList.toggle('is-active');
+    modalBtnRef.classList.toggle('is-visibility');
   });
 
-  mobileMenuClose.addEventListener("click", () => {
-    menuBtnRef.classList.toggle("is-active");
-    modalBtnRef.classList.toggle("is-visibility");
+  mobileMenuClose.addEventListener('click', () => {
+    menuBtnRef.classList.toggle('is-active');
+    modalBtnRef.classList.toggle('is-visibility');
+    document.body.classList.toggle('modal-open');
   });
 })();
